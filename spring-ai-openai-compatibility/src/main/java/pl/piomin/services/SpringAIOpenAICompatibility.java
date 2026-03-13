15105faf034ceb054a79aa7bd6084a906d5e3f9d
piomin <piotr.minkowski@gmail.com>
2026-03-13 15:48:26 +0100

    fix LMStudio connectivity issue@3

--- spring-ai-openai-compatibility/src/main/java/pl/piomin/services/SpringAIOpenAICompatibility.java
+++ spring-ai-openai-compatibility/src/main/java/pl/piomin/services/SpringAIOpenAICompatibility.java
@@ -5,6 +5,10 @@ import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpClient;
 
 @SpringBootApplication
 public class SpringAIOpenAICompatibility {
@@ -12,4 +16,13 @@ public class SpringAIOpenAICompatibility {
         SpringApplication.run(SpringAIOpenAICompatibility.class, args);
     }
 
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1) // force HTTP/1.1
+                .build();
+
+        return RestClient.builder()
+                .requestFactory(new JdkClientHttpRequestFactory(httpClient));
+    }
 }
